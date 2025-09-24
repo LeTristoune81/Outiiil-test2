0efd0da84fbc1dee2b15f2c1f3f6fe29df2788ff
LeTristoune81 <pierre.pitou81@gmail.com>
2025-09-24 13:54:14 +0200

    Update Outiiil.user.js

--- Outiiil.user.js
+++ Outiiil.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Outiiil-test2
 // @author      WhiteRabbit
-// @version     2.9
+// @version     2.9.1
 // @description Outiil de Hraesvelg Modifié par WhiteRabbit
 // @match       http://*.fourmizzz.fr/*
 // @run-at      document-end
